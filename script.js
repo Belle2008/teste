5294f4ba76201820fa46d0e6d54949c4ca392e5c
Izabelle bones <03062280093@senacrs.edu.br>
2024-08-16 11:12:34 -0300

    Script.Js

---
+++ script.js
@@ -0,0 +1,12 @@
+// perguntando ao usuario
+let nome = prompt("digite seu nome")
+alert("olá "+nome+", tudo bem ")
+
+// alerta na tela
+alert ("olá mundo")
+
+// console do navegador
+console.log("teste de console");
+
+//inclui um html em um elemento indentificado pelo ID
+document.getElementById("demo").innerHTML = "isabelle Bones";
